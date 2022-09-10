0971885d17990a4214127361b45131b0c4114709
Davies <daviesige@hotmail.com>
2022-09-10 12:54:51 -0700

    main commit

--- 0x01-variables_if_else_while/2-prints_alphabet.c
+++ 0x01-variables_if_else_while/2-prints_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
 * main - Entry point
+*
 *Description: program that prints alphabets in lowercase, then a new line
 *
 *Return: Always 0 (Success/correct)
@@ -12,7 +12,6 @@
 int main(void)
 {
 char i;
-
 for (i = 'a'; i <= 'z'; i++)
 {
 putchar(i);
