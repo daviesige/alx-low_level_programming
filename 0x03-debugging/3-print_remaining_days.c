604ed314ee65363c97437a3e0a71eb93b8527331
Davies <daviesige@hotmail.com>
2022-09-15 15:34:12 -0700

    happiness

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,7 +8,7 @@
 *@day: day of month
 *@year: year
 *Return: void
-**/
+*/
 
 void print_remaining_days(int month, int day, int year)
 {
@@ -21,14 +21,12 @@ day++;
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
-
 else
 {
 if (month == 2 && day == 60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
-
 else
 {																	printf("Day of the year: %d\n", dat);
 printf("Remaining days: %d\n", 365 - day);
